3975d24c8f582965e2bf2a18d414f5745a459396
Victoria Mertens <victoria.mertens44@gmail.com>
2023-10-30 11:15:28 -0500

    Mount clerk provider with page protection

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,18 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { store } from './src/Redux/store';
 import { Provider } from 'react-redux';
 import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabs from './src/Components/0-BottomTabs';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY =
+  pk_test_Z2VuZXJvdXMtc2x1Zy0xMC5jbGVyay5hY2NvdW50cy5kZXYk;
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <BottomTabs />
-      </NavigationContainer>
-    </Provider>
+    <ClerkProvider publishableKey={NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <Provider store={store}>
+        <SafeAreaView>
+          <SignedIn>
+            <NavigationContainer>
+              <BottomTabs />
+            </NavigationContainer>
+          </SignedIn>
+          <SignedOut></SignedOut>
+        </SafeAreaView>
+      </Provider>
+    </ClerkProvider>
   );
 }
 
