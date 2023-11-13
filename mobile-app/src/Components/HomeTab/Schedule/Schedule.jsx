297fe3cf3101c07b15bb4c4b2a232d5961d308f3
Victoria Mertens <victoria.mertens44@gmail.com>
2023-11-13 16:38:02 -0600

    Basic Tasks component imported and functioning on main Schedule page

--- mobile-app/src/Components/HomeTab/Schedule/Schedule.jsx
+++ mobile-app/src/Components/HomeTab/Schedule/Schedule.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native';
 import { MyCalendar } from './Calendar';
+import Tasks from './Tasks';
 
 export default function Schedule() {
   return (
     <View>
-      <Text>Schedule</Text>
       <MyCalendar />
+      <Tasks />
     </View>
   );
 }
---
+++ mobile-app/src/Components/HomeTab/Schedule/Tasks.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+
+export default function Tasks() {
+  return (
+    <View>
+      <Text>Testing Tasks</Text>
+    </View>
+  );
+}
