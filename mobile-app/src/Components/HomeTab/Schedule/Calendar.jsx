3e36552fc1fc6177a0a7b93f5f4bcedb344fa882
Victoria Mertens <victoria.mertens44@gmail.com>
2023-11-13 16:35:57 -0600

    Calendar UI added to project via react-native-calendars

---
+++ mobile-app/src/Components/HomeTab/Schedule/Calendar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+
+export const MyCalendar = () => {
+  let today = new Date();
+  return (
+    <View>
+      <Calendar
+        onDayPress={(day) => {
+          console.log('selected day', day);
+        }}
+        markedDates={{
+          '2023-11-13': { selected: true, marked: true },
+          '2023-06-24': { marked: true },
+          '2023-06-26': {
+            marked: true,
+            dotColor: 'red',
+            activeOpacity: 0,
+          },
+        }}
+        theme={{
+          backgroundColor: '#ffffff',
+          calendarBackground: '#ffffff',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: '#00adf5',
+          selectedDayTextColor: '#ffffff',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+          textDisabledColor: '#d9e1e8',
+          dotColor: '#00adf5',
+          selectedDotColor: '#ffffff',
+          arrowColor: '#00adf5',
+          monthTextColor: '#00adf5',
+          indicatorColor: 'blue',
+          textDayFontSize: 16,
+          textMonthFontSize: 16,
+          textDayHeaderFontSize: 16,
+        }}
+      />
+    </View>
+  );
+};
+
+//CSS for calendar from GeeksforGeeks.org
--- mobile-app/src/Components/HomeTab/Schedule/Schedule.jsx
+++ mobile-app/src/Components/HomeTab/Schedule/Schedule.jsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import { MyCalendar } from './Calendar';
 
 export default function Schedule() {
   return (
     <View>
       <Text>Schedule</Text>
+      <MyCalendar />
     </View>
   );
 }
