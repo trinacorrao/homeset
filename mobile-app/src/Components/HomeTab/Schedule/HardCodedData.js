36fc2aabdc1179614c2d9fcf89869ba59c25fc00
Victoria Mertens <victoria.mertens44@gmail.com>
2023-11-14 13:47:52 -0600

    Hard coded data to mimic backend

---
+++ mobile-app/src/Components/HomeTab/Schedule/HardCodedData.js
@@ -0,0 +1,32 @@
+export const testData = [
+  {
+    task_name: 'Garbage Day',
+    task_doer: 'Charles',
+    ini_date: new Date(),
+    frequency: 12,
+    completed: ['2023-11-01', '2023-10-01'],
+    reminder: 'Why do we have this as a string?',
+    notes: 'Place bins to the right of the driveway',
+    task_time: 0,
+  },
+  {
+    task_name: 'Yard Work',
+    task_doer: 'Amanda',
+    ini_date: new Date(),
+    frequency: 1,
+    completed: [],
+    reminder: 'Why do we have this as a string?',
+    notes: 'Both Front and back yard',
+    task_time: 16,
+  },
+  {
+    task_name: 'Plumber',
+    task_doer: 'Charles',
+    ini_date: new Date(),
+    frequency: 1,
+    completed: [],
+    reminder: 'Why do we have this as a string?',
+    notes: null,
+    task_time: 19,
+  },
+];
